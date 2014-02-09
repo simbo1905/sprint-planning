1eeac223319b488d8b3e39f6b35f0a615b3e6de9
Simon Massey <simbo1905@60hertz.com>
2014-02-09 20:54:10 +0000

    now running at www.sprint-planning.info

--- aws/aws-start.sh
+++ aws/aws-start.sh
@@ -2,4 +2,3 @@
 # You need to sudo to start the server on port 80
 cd $SPRINT_PLANNING_HOME
 nohup java -jar $SPRINT_PLANNING_HOME/target/scala-2.10/sprint-planning-runnable.jar 0.0.0.0 80 < /dev/null > $SPRINT_PLANNING_HOME/server.log &
-
